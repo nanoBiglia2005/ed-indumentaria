9c0166617822c5678a89266050bc726e0978f88e
nanoBiglia2005 <bigliastefano2005@gmail.com>
2026-09-01 11:46:46 -0300

    Prueba

--- frontend/src/components/ui/PaymentIcon.tsx
+++ frontend/src/components/ui/PaymentIcon.tsx
@@ -2,6 +2,8 @@ import { ID_METODO_TARJETA, ID_METODO_EFECTIVO } from "@backend/types";
 import { CreditCardIcon } from "@phosphor-icons/react/dist/csr/CreditCard";
 import { MoneyIcon } from "@phosphor-icons/react/dist/csr/Money";
 
+//prueba
+
 export default function PaymentIcon ({paymentId, height} : { paymentId : number, height : number}) {
     switch (paymentId){
         case ID_METODO_TARJETA:
